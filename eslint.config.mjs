cac0898e7a5f7edce78cf3f6fe490393dcb52182
Lorenz Ras <lenzras@gmail.com>
2024-12-04 00:17:01 +0900

    chore: setup linter

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import jestPlugin from 'eslint-plugin-jest';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  jestPlugin.configs['flat/all'],
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ['*.config.*',],
+  },
+
+);
