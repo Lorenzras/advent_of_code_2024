1668fa52dc3e1557aed06734fa1f76de2a0a1b41
Lorenz Ras <lenzras@gmail.com>
2024-12-05 00:10:00 +0900

    add solution day 2 part 1

--- src/day_02/part_01.test.ts
+++ src/day_02/part_01.test.ts
@@ -4,12 +4,18 @@ import path from "path";
 import f from "./part_01";
 
 describe("day 2 part 1", () => {
-  it("should return correct answer according to the sample in the problem", () => {
+  it("should return the correct answer according to the sample in the problem", () => {
     const input = fs.readFileSync(
       path.join(__dirname, "input_sample.txt"),
       "utf-8",
     );
 
     expect(f(input)).toBe(2);
+  });
+
+  it("should return the correct answer for the official input data", () => {
+    const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+    expect(f(input)).toMatchSnapshot();
   });
 });
--- src/day_02/part_01.ts
+++ src/day_02/part_01.ts
@@ -1,4 +1,44 @@
-export default function (pairs: string): number {
-  console.log("TODO: implement", pairs);
-  return 0;
+const isOneDirection = (n: number[]): boolean => {
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 0; i < n.length - 1; i++) {
+    if (n[i] > n[i + 1]) {
+      increasing = false;
+    }
+    if (n[i] < n[i + 1]) {
+      decreasing = false;
+    }
+  }
+
+  return increasing || decreasing;
+};
+
+const isSafe = (n: number[]): boolean => {
+  for (let i = 0; i < n.length - 1; i++) {
+    const curNum = n[i];
+    const nextNum = n[i + 1];
+    const distance = Math.abs(curNum - nextNum);
+
+    if (distance < 1 || distance > 3) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export default function (input: string): number {
+  const rows = input.split("\n");
+  let safe = 0;
+
+  for (const row of rows) {
+    const numbers = row.split(/\s+/).map((x) => Number(x.trim()));
+
+    if (isOneDirection(numbers) && isSafe(numbers)) {
+      safe += 1;
+    }
+  }
+
+  return safe;
 }
