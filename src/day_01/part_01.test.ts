87edba9f9c7ff8b567328ee77b4bec794c92f627
Lorenz Ras <lenzras@gmail.com>
2024-12-04 21:27:59 +0900

    chore: renamed files to prepare for day 1 part 2

--- src/day_01/part_01.test.ts
+++ src/day_01/part_01.test.ts
@@ -1,11 +1,14 @@
 import { describe, expect, it } from "@jest/globals";
 import fs from "fs";
 import path from "path";
-import f from "./solution";
+import f from "./part_01";
 
 describe("testing add file", () => {
   it("should return 11 according to sample in the problem", () => {
-    const input = ["3 4", "4 3", "2 5", "1 3", "3 9", "3 3"].join("\n");
+    const input = fs.readFileSync(
+      path.join(__dirname, "input_sample.txt"),
+      "utf-8",
+    );
 
     expect(f(input)).toBe(11);
   });
