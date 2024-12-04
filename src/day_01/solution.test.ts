02e49b6d39981d740612dcfd482afb511ec5b42d
Lorenz Ras <lenzras@gmail.com>
2024-12-04 20:23:57 +0900

    feat: add solution for day 1

--- src/day_01/solution.test.ts
+++ src/day_01/solution.test.ts
@@ -1,4 +1,6 @@
 import { describe, expect, it } from "@jest/globals";
+import fs from "fs";
+import path from "path";
 import f from "./solution";
 
 describe("testing add file", () => {
@@ -6,5 +8,17 @@ describe("testing add file", () => {
     const input = ["3 4", "4 3", "2 5", "1 3", "3 9", "3 3"].join("\n");
 
     expect(f(input)).toBe(11);
+  });
+
+  it("should return correct answer for a single pair", () => {
+    const input = "1 1";
+
+    expect(f(input)).toBe(0);
+  });
+
+  it("should return correct answer for the official input data", () => {
+    const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+    expect(f(input)).toMatchSnapshot();
   });
 });
